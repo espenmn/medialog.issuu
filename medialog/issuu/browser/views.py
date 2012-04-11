38dfc35d07537e99e3bb6556b91584f016824ce4
Espen Moe-Nilssen <espen@medialog.no>
2012-04-11 17:22:35 +0200

    Still testing the swfobject way

--- medialog/issuu/browser/views.py
+++ medialog/issuu/browser/views.py
@@ -247,8 +247,8 @@ class IssuuFlashView(BrowserView):
 
 
  	def javascript(self):
-		return u"""<h1>hallos</h1>
- <script type="text/javascript" src="http://www.theajmonline.com.au/iir/book/book1/swfobject.js" />
+ 	    
+		return """<script type="text/javascript" src="http://www.theajmonline.com.au/iir/book/book1/swfobject.js" />
         <script type="text/javascript">
                 var attributes = {
                     id: 'issuuViewer1'
