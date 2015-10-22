875a69ec5ec277075feb97d900847f0e6fc0804c
Espen Moe-Nilssen <espen@medialog.no>
2015-10-23 01:08:56 +0200

    version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(name='medialog.issuu',
       include_package_data=True,
       zip_safe=False,
       install_requires=['setuptools',
-                        'requests >= 0.9, < 1.0',
+                        'requests',
                         'medialog.controlpanel',
                         'Plone >= 5.0b4',
                         # -*- Extra requirements: -*-
